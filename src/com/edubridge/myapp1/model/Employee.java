3335bc1526ede7b330ce6eb7767416c8d2a6a493
SAI VENKAT <SAI VENKAT@saivenkat>
2024-09-24 13:12:16 +0530

    added getters and setters

--- src/com/edubridge/myapp1/model/Employee.java
+++ src/com/edubridge/myapp1/model/Employee.java
@@ -4,6 +4,24 @@ public class Employee {
 	
 	private int employee_Id;
 	private String employee_Name;
+	public int getEmployee_Id() {
+		return employee_Id;
+	}
+	public void setEmployee_Id(int employee_Id) {
+		this.employee_Id = employee_Id;
+	}
+	public String getEmployee_Name() {
+		return employee_Name;
+	}
+	public void setEmployee_Name(String employee_Name) {
+		this.employee_Name = employee_Name;
+	}
+	public double getEmployee_Salary() {
+		return employee_Salary;
+	}
+	public void setEmployee_Salary(double employee_Salary) {
+		this.employee_Salary = employee_Salary;
+	}
 	private double employee_Salary;
 }
 	
